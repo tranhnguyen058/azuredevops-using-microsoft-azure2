a2154745ea9a76318da9a332e3e50d325c057d7c
trunganh <trunganh1100@gmail.com>
2024-09-04 06:18:44 +0000

    trigger

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home TEST passed udacity </h3>"
+    html = "<h3>Sklearn Prediction Home TEST passed udacity <3 </h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
